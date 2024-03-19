730f2b91e8d66ab510b65edea97b2265399a79c1
Arthur Cabral <134966248+ArthurCR12@users.noreply.github.com>
2024-03-19 17:32:21 -0300

    Final

--- youtube/Js-FernandoLeonid/Proj-IMC/imc.js
+++ youtube/Js-FernandoLeonid/Proj-IMC/imc.js
@@ -1,24 +1,23 @@
 const calcular = document.getElementById('btn-calcular')
 const limpar = document.getElementById('btn-limpar')
-
+const resultado = document.getElementById('resultado')
 
 function limparCampos() {
     const nome = document.getElementById('txt-nome')
     const altura = document.getElementById('txt-altura')
     const peso = document.getElementById('txt-peso')
-    const resultado = document.getElementById('resultado')
     nome.value = ''
     altura.value = ''
     peso.value = ''
     resultado.textContent = ''
     nome.focus()
+    resultado.classList.remove('transformar-resultado');
 }
 
 function calcImc() {
     const nome = document.getElementById('txt-nome')
     const altura = document.getElementById('txt-altura')
     const peso = document.getElementById('txt-peso')
-    const resultado = document.getElementById('resultado')
 
     if (nome.value == '') {
         resultado.textContent = 'Peencha o nome !'
@@ -44,14 +43,29 @@ function calcImc() {
             classificacao = 'com obesidade grau I.'
         else if (valorIMC < 40)
             classificacao = 'com obesidade grau II.'
-        else 
+        else
             classificacao = 'com obesidade grau III.'
 
         resultado.textContent = (`${nome.value} seu IMC é ${valorIMC} e você está ${classificacao}`)
+
+        if (resultado.textContent.trim() !== '') {
+            resultado.classList.add('transformar-resultado');
+        }
     }
-
-
 }
 
 limpar.addEventListener('click', limparCampos)
 calcular.addEventListener('click', calcImc)
+
+resultado.addEventListener('mouseenter', function () {
+    if (resultado.innerHTML.trim() !== '') {
+        resultado.classList.add('transformar');
+    }
+});
+
+resultado.addEventListener('mouseleave', function () {
+    if (resultado.innerHTML.trim() !== '') {
+        resultado.classList.remove('transformar');
+        resultado.classList.add('voltar-normal');
+    }
+});
