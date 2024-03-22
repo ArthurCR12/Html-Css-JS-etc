3703123d0c6825940fac5f5ab599c4c639d250b2
Arthur Cabral <134966248+ArthurCR12@users.noreply.github.com>
2024-03-22 14:11:17 -0300

    Finalização do proj

--- youtube/Js-FernandoLeonid/Proj-DrumKit/drum.js
+++ youtube/Js-FernandoLeonid/Proj-DrumKit/drum.js
@@ -9,7 +9,7 @@ const sons = {
     'G': 'openhat.wav',
     'H': 'ride.wav',
     'J': 'snare.wav',
-    'k': 'tink.wav',
+    'K': 'tink.wav',
     'L': 'tom.wav',
 }
 
@@ -23,23 +23,31 @@ const criarDiv = (texto) => {
 
 const exibir = (sons) => Object.keys(sons).forEach(criarDiv)
 
-const tocarSom = (letra) =>{
+const tocarSom = (letra) => {
     const som = new Audio(`./assets/sounds/${sons[letra]}`)
     som.play()
 }
 
 const adicionarEfeito = (letra) => document.getElementById(letra).classList.add('active')
-const removerEfeito = (letra) => document.getElementById(letra).classList.remove('active')
 
-const ativarSom = (evento) => {
-    const letra = evento.target.id
+const removerEfeito = (letra) => {
+    const div = document.getElementById(letra)
+    const removeActive = () => document.getElementById(letra).classList.remove('active')
+    div.addEventListener('transitionend', removeActive)    
+}
+
+const ativarSom = (evento) => {    
+    const letra = evento.type == 'click' ? evento.target.id : evento.key.toUpperCase();
     const letraPermitida = sons.hasOwnProperty(letra)
     if (letraPermitida)
         adicionarEfeito(letra)
-        tocarSom(letra)
-        removerEfeito(letra)
+    tocarSom(letra)
+    removerEfeito(letra)
 }
 
 exibir(sons)
 
 document.getElementById('container').addEventListener('click', ativarSom)
+
+
+window.addEventListener('keydown', ativarSom)
