7543d2db77d9db03a37133364460a07c5acac1db
Arthur Cabral <134966248+ArthurCR12@users.noreply.github.com>
2024-04-15 12:32:49 -0300

    update

--- Projetos/weathersite/main.js
+++ Projetos/weathersite/main.js
@@ -1,10 +1,12 @@
-document.addEventListener("DOMContentLoaded", () => {    
+document.addEventListener("DOMContentLoaded", () => {
     const inputCity = document.getElementById('input-city')
     const txtCel = document.getElementById('cel')
     const txtFah = document.getElementById('fah')
     const txtHumi = document.getElementById('humidity')
     const txtDescr = document.getElementById('description')
+    const txtCity = document.getElementById('city')
     const apiKey = 'd6e3a416459d4b7daf7b6b0854bfdde6'
+    const cbLang = document.getElementById('check')
 
     async function fetchWeather(city) {
 
@@ -17,8 +19,8 @@ document.addEventListener("DOMContentLoaded", () => {
             }
             if (city == 'City') {
                 throw new Error("Please select a country or city!")
-            }          
-            console.log(apiUrl)  
+            }
+            console.log(apiUrl)
             return await response.json()
         }
         catch (error) {
@@ -31,6 +33,7 @@ document.addEventListener("DOMContentLoaded", () => {
         const tempCelsius = temp - 273.15
         const tempFahrenheit = (tempCelsius / 0.5566) + 32
 
+        txtCity.textContent = city
         txtCel.textContent = `Celsius ${tempCelsius.toFixed(1)}°`
         txtFah.textContent = `Fahrenheit ${tempFahrenheit.toFixed(1)}°`
         txtHumi.textContent = `Humidity ${humidity}%`
@@ -41,11 +44,12 @@ document.addEventListener("DOMContentLoaded", () => {
     async function temp() {
         try {
             const city = inputCity.value.trim()
+
             if (!city) {
                 alert("Please enter a valid city name")
             }
             const weather = await fetchWeather(city)
-            displayWeather(weather)            
+            displayWeather(weather)
         }
 
         catch (error) {
@@ -53,10 +57,10 @@ document.addEventListener("DOMContentLoaded", () => {
         }
     }
 
-    const changeBack = (temperature) => {        
-        const backG = document.getElementById('bd')  
-        for(const range of temperatureRange) {
-            if(temperature >= range.min && temperature <= range.max){
+    const changeBack = (temperature) => {
+        const backG = document.getElementById('bd')
+        for (const range of temperatureRange) {
+            if (temperature >= range.min && temperature <= range.max) {
                 backG.style.background = range.color
                 break;
             }
@@ -64,83 +68,30 @@ document.addEventListener("DOMContentLoaded", () => {
     }
 
     const temperatureRange = [
-        {min: 29, max: 100, color: 'linear-gradient(162deg, rgba(255,209,0,1) 18%, rgba(255,112,0,1) 51%)'},
-        {min: 18, max: 29, color:'linear-gradient(151deg, rgba(224,255,0,1) 17%, rgba(0,164,255,1) 26%)'},
-        {min: 10, max: 18, color: 'linear-gradient(150deg, rgba(4,157,191,1) 9%, rgba(5,219,242,1) 34%)'},
-        {min: 0, max: 10, color: 'linear-gradient(185deg, rgba(61,115,135,1) 0%, rgba(139,187,217,1) 39%)'},
-        {min: -100, max: 0, color: 'linear-gradient(0deg, rgba(129,138,150,1) 9%, rgba(223,225,232,1) 57%)'}
+        { min: 29, max: 100, color: 'linear-gradient(162deg, rgba(255,209,0,1) 18%, rgba(255,112,0,1) 51%)' },
+        { min: 18, max: 29, color: 'linear-gradient(151deg, rgba(224,255,0,1) 17%, rgba(0,164,255,1) 26%)' },
+        { min: 10, max: 18, color: 'linear-gradient(150deg, rgba(4,157,191,1) 9%, rgba(5,219,242,1) 34%)' },
+        { min: 0, max: 10, color: 'linear-gradient(185deg, rgba(61,115,135,1) 0%, rgba(139,187,217,1) 39%)' },
+        { min: -100, max: 0, color: 'linear-gradient(0deg, rgba(129,138,150,1) 9%, rgba(223,225,232,1) 57%)' }
 
     ]
 
+    const changeLang = () => {
+        if (cbLang.checked) {
+            console.log('checked')
+            txtCity.textContent = 'Cidade'            
+            txtHumi.textContent = `Humidade `
+            txtDescr.textContent = 'Descrição'
+        }
+        else {
+            console.log('not checked')
+            txtCity.textContent = "City"            
+            txtHumi.textContent = `Humidity`
+            txtDescr.textContent = 'Description'
+        }
+    }
+
     document.getElementById('find').addEventListener('click', temp)
+    cbLang.addEventListener('click', changeLang)
 })
 
-
-
-async function fetchCountries() {
-    const url = 'https://country-state-city-search-rest-api.p.rapidapi.com/allcountries';
-    const options = {
-        method: 'GET',
-        headers: {
-            'X-RapidAPI-Key': '99d14885f1msh7d2906601274b6cp1f4abajsna3374652a9f6',
-            'X-RapidAPI-Host': 'country-state-city-search-rest-api.p.rapidapi.com'
-        }
-    };
-
-    try {
-        const response = await fetch(url, options);
-        const result = await response.json();
-        const inputCountry = document.getElementById('input-country')
-
-        result.forEach(country => {
-            const option = document.createElement('option')
-            option.value = country.isoCode
-            option.text = country.name
-            inputCountry.add(option)
-        })
-    } catch (error) {
-        console.error(error);
-    }
-}
-
-document.getElementById('input-country').addEventListener('change', function () {
-    if (this.value !== 'Country') {
-        const selectedCountry = this.options[this.selectedIndex].text
-        //fetchCities(this.value)
-        document.getElementById('country').textContent = selectedCountry
-    }
-})
-
-async function fetchCities(id_country) {
-    const url = `https://country-state-city-search-rest-api.p.rapidapi.com/cities-by-countrycode?countrycode=${id_country}`;
-    const options = {
-        method: 'GET',
-        headers: {
-            'X-RapidAPI-Key': '99d14885f1msh7d2906601274b6cp1f4abajsna3374652a9f6',
-            'X-RapidAPI-Host': 'country-state-city-search-rest-api.p.rapidapi.com'
-        }
-    };
-
-    try {
-        const response = await fetch(url, options);
-        const result = await response.json();
-
-        const inputCity = document.getElementById('input-city')
-        inputCity.innerHTML = '';
-
-        result.forEach(cities => {
-            const option = document.createElement('option')
-            option.value = cities.name
-            option.text = cities.name
-            inputCity.add(option)
-        })
-    } catch (error) {
-        console.error(error);
-    }
-}
-
-document.getElementById('input-city').addEventListener('change', function () {
-    document.getElementById('city').textContent = this.value
-})
-
-//window.document.onload = fetchCities
