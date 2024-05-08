d6eed3a3b835776a79ced1a03dca439e19b6340d
Arthur Cabral <134966248+ArthurCR12@users.noreply.github.com>
2024-05-08 16:42:46 -0300

    update

--- youtube/freeCodeCamp/colorFlipper/app.js
+++ youtube/freeCodeCamp/colorFlipper/app.js
@@ -2,16 +2,35 @@ const colors = ['green', 'red', 'rgba(133,122,200)', '#f15025', 'yellow']
 const btn = document.getElementById('btn')
 const color = document.querySelector('.color')
 
-
-btn.addEventListener('click', function(){
+btn.addEventListener('click', function () {
     // Pegar um número entre 0 e 3 por causa do array colors    
     const randomNumber = getRandomNumber()
     console.log(randomNumber)
     const body = document.body
     body.style.background = colors[randomNumber]
     color.textContent = colors[randomNumber]
+    color.style.color = colors[randomNumber]
 })
 
-function getRandomNumber(){
-    return Math.floor(Math.random()*colors.length);
+function getRandomNumber() {
+    return Math.floor(Math.random() * colors.length);
+}
+
+function changeFocus(element) {
+    const simple = document.getElementById('simple')
+    const hexType = document.getElementById('hex')
+
+    if (element === 'simple') {
+        simple.classList.add('active')
+        hexType.classList.remove('active')
+        simple.classList.remove('normal')
+        hexType.classList.add('normal')
+        hexType.classList.remove('active')
+    } else if (element === 'hex') {
+        hexType.classList.add('active')
+        simple.classList.remove('active')
+        hexType.classList.remove('normal')
+        simple.classList.add('normal')
+        simple.classList.remove('active')
+    }
 }
--- youtube/freeCodeCamp/colorFlipper/hex.js
+++ youtube/freeCodeCamp/colorFlipper/hex.js
@@ -1,17 +1,40 @@
 const hex = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, "A", 'B', 'C', 'D', 'E', 'F']
-
 const btn = document.getElementById('btn')
 const color = document.querySelector('.color')
 
-btn.addEventListener('click', function(){
+const simple = document.getElementById('simple')
+const hexType = document.getElementById('hex')
+
+btn.addEventListener('click', function () {
     let hexColor = '#'
-    for(let i = 0; i < 6; i++){
+    for (let i = 0; i < 6; i++) {
         //Math.floor(Math.random() * hex)
-        hexColor +=  hex[Math.floor(Math.random() * hex)]
-        
+        hexColor += hex[Math.floor(Math.random() * hex.length)]
+
     }
     const body = document.body
     body.style.background = hexColor
     color.textContent = hexColor
-    console.log(hexColor)
+    color.style.color = hexColor
 })
+
+
+function changeFocus(element) {
+    const simple = document.getElementById('simple')
+    const hexType = document.getElementById('hex')
+
+    if (element === 'simple') {
+        simple.classList.add('active')
+        hexType.classList.remove('active')
+        simple.classList.remove('normal')
+        hexType.classList.add('normal')
+        hexType.classList.remove('active')
+    } else if (element === 'hex') {
+        hexType.classList.add('active')
+        simple.classList.remove('active')
+        hexType.classList.remove('normal')
+        simple.classList.add('normal')
+        simple.classList.remove('active')
+    }
+}
+
